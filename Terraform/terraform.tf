95c7a36add7bea974fb4c9b54027042e835ffdab
prashob <vp.prashob@gmail.com>
2024-11-14 07:43:38 +0100

    a

---
+++ Terraform/terraform.tf
@@ -0,0 +1,43 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  backend "s3" {
+    bucket = "terra-eks-clu-bucket"
+    key    = "LockID"
+    region = "us-east-1"
+    dynamodb_table = "eks-terra-table"
+  }
+
+  # cloud {
+  #   workspaces {
+  #     name = "learn-terraform-eks"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
