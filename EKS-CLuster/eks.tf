5e7704322bcc94c358f31bf84aaaec1eea79e6e2
Nishant Nath <108796288+amigo-nishant@users.noreply.github.com>
2024-12-02 17:34:16 +0530

    Update eks.tf

--- EKS-CLuster/eks.tf
+++ EKS-CLuster/eks.tf
@@ -1,30 +1,48 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.29.0"
-  cluster_name    = local.cluster_name
-  cluster_version = var.kubernetes_version
-  subnet_ids      = module.vpc.private_subnets
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.30.1"
 
-  enable_irsa = true
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  tags = {
-    cluster = "MERN-K8s-cluster"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
+  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64"
-    instance_types         = ["t3.medium"]
-    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+
+    attach_cluster_primary_security_group = true
   }
 
   eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-    node_group = {
-      min_size     = 2
-      max_size     = 6
-      desired_size = 2
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "cluster"
+      }
     }
   }
+
+  tags = local.tags
 }
